37e040a54cb507b53d6a7b13fd2afb305e3bfd48
AlbertoMontalesi <alberto.montalesi@outlook.it>
2017-06-21 15:34:56 +0700

    add select response partial

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,9 +1,10 @@
 class JobsController < ApplicationController
   respond_to :html, :js, :json
-
+  
   def index
     @jobs = Job.all.order(created_at: :desc)
     # @job= Job.find params[:id]
+    
   end
 
   def new
@@ -25,12 +26,20 @@ class JobsController < ApplicationController
 
   def update
     @job = Job.find(params[:id])
+    respond_to do |format|               
+      format.js
+    end 
     unless @job.update_attributes(job_params)
       redirect_to root_path
     end
+
   end
 
   private
+
+  # unless the user selects interview stage we automatically set the interview stage so it wont display
+ 
+
 
   def job_params
     params.require(:job).permit(:company, :role, :salary, :application_date, :application_closing_date, :contact_name, :contact_email, :contact_phone, :description, :link, :response, :interview_stage, :notes)
