411ff800033479b11f9731a2c2e40bbdb7044a4d
AlbertoMontalesi <alberto.montalesi@outlook.it>
2017-06-29 17:52:26 +0700

    style user profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 
+  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,19 @@ class User < ApplicationRecord
   validates_uniqueness_of :username, :email
   
   has_many :jobs
+
+  # jobs with  response
+  def response_jobs
+    self.jobs.where.not(response: 'no_response').count
+  end
+
+#jobs refused
+  def refused_jobs
+    self.jobs.where(response: 'refused').count
+  end
+#jobswith interview 
+  def interview_jobs
+    self.jobs.where(response: 'interview').count
+  end
+
 end
